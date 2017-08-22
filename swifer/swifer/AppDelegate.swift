235f51beb4443c1b88b12aa7aee4633eb7bcd04d
jenix21 <devfrog@gmail.com>
2017-08-23 00:02:32 +0900

    Initial

---
+++ swifer/swifer/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  swifer
+//
+//  Created by jongdeok.kim on 2017. 8. 22..
+//  Copyright © 2017년 jongdeok.kim. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ swifer/swifer/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  swifer
+//
+//  Created by jongdeok.kim on 2017. 8. 22..
+//  Copyright © 2017년 jongdeok.kim. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
